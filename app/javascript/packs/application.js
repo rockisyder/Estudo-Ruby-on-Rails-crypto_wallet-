681808ca2ddccef5636efefed345e4bb30329a2c
jorlan nascimento <jorlan.hora@hotmail.com>
2021-08-31 23:50:39 +0000

    init

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,5 +14,5 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
+//= require jquery/dist/jquery
 console.log('Hello World from Webpacker')
